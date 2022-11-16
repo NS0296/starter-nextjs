fc9c905c74f715702af0256b18ba99d9fbb4c9c0
NS0296 <110940186+NS0296@users.noreply.github.com>
2022-11-16 20:50:51 -0300

    Create axios.js

---
+++ pages/api/axios.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+const API_ENDPOINT = 'http://nuevomundo.tk'
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await axios.get(API_ENDPOINT)
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response.data
+    })
+  }
+}

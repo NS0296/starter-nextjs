d4e889aa5964d982ea852fc98f6362a9f20d9a6a
NS0296 <110940186+NS0296@users.noreply.github.com>
2022-11-17 22:38:51 -0300

    Update axios.js

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -7,12 +7,9 @@ const API_ENDPOINT = 'http://nuevomundo.tk'
 
 export default function handler(req, res) {
   axios({
-    method: 'post',
-    url: 'http://nuevomundo.tk',
-    data: {
-      firstName: 'Fred',
-      lastName: 'Flintstone'
-    }
+    method: 'get',
+    url: 'https://cs.nuevomundo.tk'
+    
   }).then(res => {
       consloe.log(res)
        res.status(200).json({ name: 'John Doe' })
